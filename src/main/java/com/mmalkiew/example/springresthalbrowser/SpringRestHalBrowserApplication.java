da5145351e67768b194c928b189f1c3bdf62dc0f
Michał Małkiewicz <michal.malkiewicz@uniteam.pl>
2019-09-02 22:40:24 +0200

    init spring rest and HAL browser project

---
+++ src/main/java/com/mmalkiew/example/springresthalbrowser/SpringRestHalBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.mmalkiew.example.springresthalbrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestHalBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestHalBrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmalkiew/example/springresthalbrowser/SpringRestHalBrowserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mmalkiew.example.springresthalbrowser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestHalBrowserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
